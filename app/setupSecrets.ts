11ab15c9527b54df8a9725f39389b9a861111000
Brad Sorour <brad.sorour@gmail.com>
2020-05-14 15:21:42 +0100

    removed ia-redis-access-key config

--- app/setupSecrets.ts
+++ app/setupSecrets.ts
@@ -15,8 +15,6 @@ const setupSecrets = () => {
   setSecret('secrets.ia.idam-secret', 'idam.secret');
   setSecret('secrets.ia.addressLookupToken', 'addressLookup.token');
   setSecret('secrets.ia.s2s-secret', 's2s.secret');
-  setSecret('secrets.ia.ia-redis-connection-string', 'session.redis.url');
-  setSecret('secrets.ia.ia-redis-access-key', 'session.redis.secret');
   setSecret('secrets.ia.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
 
   return config;
