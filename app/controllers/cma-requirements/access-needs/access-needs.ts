9369476b6e0bc4e59a5e3732f770868a0f44095f
Lewis Williams <lewis.williams@transformuk.com>
2020-06-08 16:59:03 +0100

    RIA-3100-save-for-later

--- app/controllers/cma-requirements/access-needs/access-needs.ts
+++ app/controllers/cma-requirements/access-needs/access-needs.ts
@@ -5,6 +5,7 @@ import { Events } from '../../../data/events';
 import { isoLanguages } from '../../../data/isoLanguages';
 import { paths } from '../../../paths';
 import UpdateAppealService from '../../../service/update-appeal-service';
+import { shouldValidateWhenSaveForLater } from '../../../utils/save-for-later-utils';
 import { getConditionalRedirectUrl } from '../../../utils/url-utils';
 import { selectedRequiredValidation, yesOrNoRequiredValidation } from '../../../utils/validations/fields-validations';
 
@@ -44,6 +45,10 @@ function getNeedInterpreterPage(req: Request, res: Response, next: NextFunction)
 function postNeedInterpreterPage(updateAppealService: UpdateAppealService) {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
+
+      if (!shouldValidateWhenSaveForLater(req.body, 'answer')) {
+        return getConditionalRedirectUrl(req, res, paths.common.overview + '?saved');
+      }
       const validation = yesOrNoRequiredValidation(req.body, i18n.validationErrors.cmaRequirements.accessNeeds.selectInterpreter);
       const answer = req.body.answer === 'yes' ? true : false;
       if (validation) {
@@ -92,6 +97,9 @@ function getAdditionalLanguage(req: Request, res: Response, next: NextFunction) 
 function postAdditionalLanguage(updateAppealService: UpdateAppealService) {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
+      if (!shouldValidateWhenSaveForLater(req.body, 'language','dialect')) {
+        return getConditionalRedirectUrl(req, res, paths.common.overview + '?saved');
+      }
       const validation = selectedRequiredValidation(req.body,i18n.validationErrors.cmaRequirements.accessNeeds.addLanguage);
       if (validation) {
         return res.render('case-management-appointment/additional-language.njk', {
@@ -139,6 +147,9 @@ function getStepFreeAccessPage(req: Request, res: Response, next: NextFunction) 
 function postStepFreeAccessPage(updateAppealService: UpdateAppealService) {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
+      if (!shouldValidateWhenSaveForLater(req.body, 'answer')) {
+        return getConditionalRedirectUrl(req, res, paths.common.overview + '?saved');
+      }
       const { isHearingRoomNeeded, isInterpreterServicesNeeded } = req.session.appeal.cmaRequirements.accessNeeds;
       const backButton = isInterpreterServicesNeeded === true ? paths.awaitingCmaRequirements.accessNeedsAdditionalLanguage : paths.awaitingCmaRequirements.accessNeedsInterpreter;
       const answer = isHearingRoomNeeded || null;
@@ -192,6 +203,9 @@ function getHearingLoopPage(req: Request, res: Response, next: NextFunction) {
 function postHearingLoopPage(updateAppealService: UpdateAppealService) {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
+      if (!shouldValidateWhenSaveForLater(req.body, 'answer')) {
+        return getConditionalRedirectUrl(req, res, paths.common.overview + '?saved');
+      }
       const { isHearingLoopNeeded } = req.session.appeal.cmaRequirements.accessNeeds;
       const answer = isHearingLoopNeeded || null;
       const validation = yesOrNoRequiredValidation(req.body, i18n.validationErrors.cmaRequirements.accessNeeds.hearingLoop);
